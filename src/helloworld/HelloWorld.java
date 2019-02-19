c61349af41ad6c6df4fcbfbacb6d25e0cd432b16
Ida <ida.maria.solli@gmail.com>
2019-02-19 11:23:10 +0100

    Initial commit

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,15 @@
+
+package helloworld;
+
+/**
+ *
+ * @author Ida Maria Solli
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
